dc61996acca8a35deb4e4e5cd844ce7b50170bd1
Vipul <vipul.sharma20@gmail.com>
2016-11-15 02:04:07 +0530

    add docstrings

--- app/util.py
+++ app/util.py
@@ -2,10 +2,36 @@ from nltk.stem.porter import PorterStemmer
 from nltk.stem import SnowballStemmer
 from nltk.stem.lancaster import LancasterStemmer
 
+from nltk import word_tokenize
+from nltk.tokenize import TweetTokenizer
+
+from app.constants import DEFAULT_STEMMER
 
 class NLTKStem(object):
     """
     NLTK Stemmers used: Porter, Snowball and Lancaster
+
+    Accepts:
+
+    /api/stem?word=<word>/
+    /api/stem?word=<word>&stemmer=<porter/snowball/lancaster>/
+    /api/stem?word=<word>&stemmer=snowball&language=<language>&ignore_stopwords=<true/false>/
+
+    Query Parameters:
+
+        * Mandatory:
+            1. word:
+                type: string
+        * Optional:
+            1. stemmer:
+                value: porter/snowball/lancaster
+                default: snowball
+            2. ignore_stopwords: Only for Snowball Stemmer
+                value: true/false
+                default: false
+            3. language: Only for Snowball Stemmer
+                value: see SnowballStemmer.languages
+                default: english
     """
 
     dispatch = {
@@ -18,10 +44,11 @@ class NLTKStem(object):
         self.options = options
 
     def stem(self):
-        stemmer = self.options.get('stemmer', 'snowball')
-        stemmer_obj = self.dispatch[stemmer]
+        stemmer = self.options.get('stemmer', DEFAULT_STEMMER)
+        stemmer_obj = self.dispatch.get(stemmer, self.dispatch[DEFAULT_STEMMER])
 
-        if stemmer == 'snowball':
+        print dir(stemmer_obj)
+        if stemmer_obj == SnowballStemmer:
             ignore_stopwords = False
 
             if self.options.get('ignore_stopwords'):
@@ -43,3 +70,11 @@ class NLTKStem(object):
                 }
         return response
 
+
+class NLTKTokenize(object):
+
+    def __init__(self, options):
+        self.options = options
+
+    def tokenize(self):
+        print 'something'
