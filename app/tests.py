54b8f1b6257174b43272d9d6eca1d27b07160a70
Vipul <vipul.sharma20@gmail.com>
2016-11-29 17:03:13 +0530

    Revert "add tests"
    This reverts commit cf054b8eff526417965bc1c4399c030d388eb2c5.

--- app/tests.py
+++ app/tests.py
@@ -1,32 +1,3 @@
-from django.test import TestCase, Client
-from django.contrib.auth.models import User
+from django.test import TestCase
 
-from app.views import StemView
-from app.models import Product
-
-from rest_framework import status
-from rest_framework.test import APIRequestFactory, APIClient, force_authenticate
-import json
-
-class ProductTest(TestCase):
-
-    def test_stem_no_auth(self):
-
-        c = APIClient()
-        data = [{'words': 'generous,religious,something,consumer'},
-                {'words': 'generous,religious,something,consumer',
-                'stemmer': 'porter'}]
-        for d in data:
-            response = client.get('/api/stem/', data, format='json')
-            self.asserEqual(resoonse.status_code, status.HTTP_20)_OK)
-
-    def test_tokenize_no_auth(self):
-
-        c = APIClient()
-        data = [{'sentence': 'generous religious something consumer'},
-                {'sentence': '#generous #religious something consumer',
-                'tokenizer': 'tweet'}]
-        for d in data:
-            response = client.get('/api/tokenize/', data, format='json')
-            self.asserEqual(resoonse.status_code, status.HTTP_20)_OK)
-
+# Create your tests here.
