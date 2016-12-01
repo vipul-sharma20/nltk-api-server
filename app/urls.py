77b21cda9bdde47d529b288df4e3d2553cce9a35
Vipul <vipul.sharma20@gmail.com>
2016-12-01 22:57:04 +0530

    add /api/ doc

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
-from app.views import StemView, TokenizeView, POSTagView, NERView
+from app.views import StemView, TokenizeView, POSTagView, NERView, HomeView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+        url(r'^api/$', HomeView.as_view()),
         url(r'^api/stem/$', StemView.as_view()),
         url(r'^api/tokenize/$', TokenizeView.as_view()),
-        url(r'api/tag/$', POSTagView.as_view()),
-        url(r'api/ner/$', NERView.as_view()),
+        url(r'^api/tag/$', POSTagView.as_view()),
+        url(r'^api/ner/$', NERView.as_view()),
         ]
 
 urlpatterns += format_suffix_patterns(urlpatterns)
