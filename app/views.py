07566e1fb4ab3e9031c2726abdc5e5a04789701a
Vipul <vipul.sharma20@gmail.com>
2016-12-03 01:27:13 +0530

    add middleware for validation

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from util import NLTKStem, NLTKTokenize, NLTKTag, NLTKner
+from util import NLTKStem, NLTKTokenize, NLTKTag, NLTKner, NLTKLemmatize
 
 
 class HomeView(APIView):
@@ -37,16 +37,9 @@ class StemView(APIView):
 
     def get(self, request):
         data = request.GET
-
-        if not data.get('words'):
-            return Response({
-                'message': 'words parameter missing',
-                'status': False,
-            })
-
         stem_obj = NLTKStem(data)
-
         res = stem_obj.stem()
+
         return Response(res)
 
 
@@ -57,13 +50,6 @@ class TokenizeView(APIView):
 
     def get(self, request):
         data = request.GET
-
-        if not data.get('sentence'):
-            return Response({
-                'message': 'sentence parameter missing',
-                'status': False
-            })
-
         tokenize_obj = NLTKTokenize(data)
         res = tokenize_obj.tokenize()
 
@@ -77,13 +63,6 @@ class POSTagView(APIView):
 
     def get(self, request):
         data = request.GET
-
-        if not data.get('sentence'):
-            return Response({
-                'message': 'sentence parameter missing',
-                'status': False
-            })
-
         pos_obj = NLTKTag(data)
         res = pos_obj.pos_tag()
 
@@ -97,15 +76,20 @@ class NERView(APIView):
 
     def get(self, request):
         data = request.GET
-
-        if not data.get('sentence'):
-            return Response({
-                'message': 'sentence parameter missing',
-                'status': False
-            })
-
         ner_obj = NLTKner(data)
         res = ner_obj.ner()
 
         return Response(res)
 
+
+class LemmatizeView(APIView):
+    """
+    View for Lemmatization
+    """
+
+    def get(self, request):
+        data = request.GET
+        lemma_obj = NLTKLemmatize(data)
+        res = lemma_obj.lemma()
+
+        return Response(res)
